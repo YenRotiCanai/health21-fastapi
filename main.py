96a8ba055b36b30d73e5feaad0b55a707daf90a7
YenRotiCanai <lnight94@gmail.com>
2021-11-17 18:22:28 +0800

    soHardQQ

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+
+import requests
+import json
+import urllib.request as ur
+from urllib.parse import quote
+import string
+
+import ortools
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
     return {"message":"hihi"}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {
+        "filename" : file.filename
+    }
