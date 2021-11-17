985a03ff9f683670a7cba802db0a0f4b1eb07699
YenRotiCanai <lnight94@gmail.com>
2021-11-17 18:30:27 +0800

    a

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+
+import requests
+import json
+import urllib.request as ur
+from urllib.parse import quote
+import string
+
+import ortools
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+import pandas as pd
 
 app = FastAPI()
 
